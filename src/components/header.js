52753b08e6eb85c30a7e6bf87e4f57878ee3fb69
Alexander Tsarapkine <alexander.tsarapkine@gmail.com>
2020-10-02 19:27:08 -0400

    Implemented React Framework
    need to move previous elements to this

---
+++ src/components/header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>HEADER</h1>
+                <ul>
+                    <li>
+                        HOME
+                    </li>
+                    <li>
+                        ABOUT
+                    </li>
+                    <li>
+                        SERVICES
+                    </li>
+                    <li>
+                        PORTFOLIO
+                    </li>
+                    <li>
+                        PROCESS
+                    </li>
+                    <li>
+                        CONTACT
+                    </li>
+                    <li>
+                        EN
+                    </li>
+                    <li>
+                        |
+                    </li>
+                    <li>
+                        SR
+                    </li>
+                </ul>
+        </header>
+    )
+}
+
+export default Header
