bd8df3c3564279539729d3acd46a07ff74706242
lukagra <lukagrahek2003@gmail.com>
2020-10-02 21:51:54 -0400

    Added Homepage files and Images

---
+++ LDesignWeb/src/components/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import bed from '../photos/1bed.jpg'
+import '../styles/homepageStyle.css'
+
+
+const HomeSlide = () => {
+    return(
+        <img src={bed} alt="Beautifully designed Bed portrait" />
+    )
+}
+
+export default HomeSlide
--- LDesignWeb/src/pages/index.js
+++ LDesignWeb/src/pages/index.js
@@ -1,12 +1,16 @@
 import React from "react"
 
 import Layout from '../components/layout'
+import HomeSlide from '../components/homepage'
+
+import '../styles/indexStyle.css'
 
 const IndexPage = () => {
 
   return (
     <Layout>
       <h1>howdy</h1>
+      <HomeSlide/>
     </Layout>
   )
 }
