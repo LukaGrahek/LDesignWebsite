b91a2a031ba3ad446e8f0292061e3ab5e3ef7dd5
Luka <lukagrahek2003@gmail.com>
2020-10-08 11:35:01 -0400

    fixed Projects Section Posistioning

--- src/components/projects.js
+++ src/components/projects.js
@@ -7,78 +7,80 @@ const Projects = () => {
     return(
         <Jumbotron>
             <h1 id="ProjTitle">Projects</h1>
-            <div class="row">
-                <div class="column">
-                    <div class="hovereffect">
-                         <img class="img-responsive" id="left" src={fill} alt=""/>
-                         <div class="overlay">
-                            <h2>project Name</h2>
-                            <a class="info" href="#">View Project</a>
+            <div id="projectsTABLE">
+                <div class="row">
+                    <div class="column">
+                        <div class="hovereffect">
+                            <img class="img-responsive" id="left" src={fill} alt=""/>
+                            <div class="overlay">
+                                <h2>project Name</h2>
+                                <a class="info" href="">View Project</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="hovereffect">
+                            <img class="img-responsive" src={fill} alt=""/>
+                            <div class="overlay">
+                                <h2>project Name</h2>
+                                <a class="info" href="">View Project</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="hovereffect">
+                            <img class="img-responsive" src={fill} alt=""/>
+                            <div class="overlay">
+                                <h2>project Name</h2>
+                                <a class="info" href="">View Project</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="hovereffect">
+                            <img class="img-responsive" src={fill} alt=""/>
+                            <div class="overlay">
+                                <h2>project Name</h2>
+                                <a class="info" href="">View Project</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="column">
-                    <div class="hovereffect">
-                        <img class="img-responsive" src={fill} alt=""/>
-                        <div class="overlay">
-                            <h2>project Name</h2>
-                            <a class="info" href="#">View Project</a>
+                <div class="row">
+                    <div class="column">
+                        <div class="hovereffect">
+                            <img class="img-responsive" id="left2" src={fill} alt=""/>
+                            <div class="overlay">
+                                <h2>project Name</h2>
+                                <a class="info" href="">View Project</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="column">
-                    <div class="hovereffect">
-                        <img class="img-responsive" src={fill} alt=""/>
-                        <div class="overlay">
-                            <h2>project Name</h2>
-                            <a class="info" href="#">View Project</a>
-                         </div>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="hovereffect">
-                        <img class="img-responsive" src={fill} alt=""/>
-                        <div class="overlay">
-                            <h2>project Name</h2>
-                            <a class="info" href="#">View Project</a>
-                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="column">
-                    <div class="hovereffect">
-                        <img class="img-responsive" id="left2" src={fill} alt=""/>
-                        <div class="overlay">
-                            <h2>project Name</h2>
-                            <a class="info" href="#">View Project</a>
+                    <div class="column">
+                        <div class="hovereffect">
+                            <img class="img-responsive" src={fill} alt=""/>
+                            <div class="overlay">
+                                <h2>project Name</h2>
+                                <a class="info" href="">View Project</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="column">
-                    <div class="hovereffect">
-                         <img class="img-responsive" src={fill} alt=""/>
-                        <div class="overlay">
-                            <h2>project Name</h2>
-                            <a class="info" href="#">View Project</a>
+                    <div class="column">
+                        <div class="hovereffect">
+                            <img class="img-responsive" src={fill} alt=""/>
+                            <div class="overlay">
+                                <h2>project Name</h2>
+                                <a class="info" href="">View Project</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="column">
-                    <div class="hovereffect">
-                        <img class="img-responsive" src={fill} alt=""/>
-                        <div class="overlay">
-                            <h2>project Name</h2>
-                            <a class="info" href="#">View Project</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="hovereffect">
-                        <img class="img-responsive" src={fill} alt=""/>
-                        <div class="overlay">
-                             <h2>project Name</h2>
-                             <a class="info" href="#">View Project</a>
+                    <div class="column">
+                        <div class="hovereffect">
+                            <img class="img-responsive" src={fill} alt=""/>
+                            <div class="overlay">
+                                <h2>project Name</h2>
+                                <a class="info" href="">View Project</a>
+                            </div>
                         </div>
                     </div>
                 </div>
