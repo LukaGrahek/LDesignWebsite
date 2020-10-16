f125b6f6f567f45de9884eb7a38ae1268562866f
Alexander Tsarapkine <alexander.tsarapkine@gmail.com>
2020-10-16 10:04:24 -0400

    Made submit button save values in contact section ( not yet to databse)

--- Frontend/src/components/contact.js
+++ Frontend/src/components/contact.js
@@ -1,38 +1,93 @@
 import React from 'react';
 import '../styles/contactStyle.css'
 
-const Contact = () => {
-    return (
+export default class Contact extends React.Component {
+
+    state = {
+        name: "",
+        email: "",
+        tel: "",
+        mes: ""
+      }
+
+    handleInputChange = event => {
+        const target = event.target
+        const value = target.value
+        const name = target.name
+        this.setState({
+          [name]: value,
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        alert(`Welcome ${this.state.name} ${this.state.email} ${this.state.tel} ${this.state.mes}!`)
+      }
+
+    render() {
+        return (
         <div class='contactSection'>
             <h1>Contact Us</h1>
-            <table id='contactSubmission'>
-                <tr>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                </tr>
-                <tr>
+            <form onSubmit={this.handleSubmit}>
+                <table id='contactSubmission'>
+                   <tr>
+                     <th></th>
+                     <th></th>
+                     <th></th>
+                 </tr>
+                 <tr>
                     <th>
-                    <input type="text" id="Name" placeholder='Your Name'></input><br></br>
-                    <input type="email" id="Email" placeholder='Your Email'></input><br></br>
-                    <input type="tel" id="Phone" placeholder='Your Phone'></input><br></br>
-                    <input type="submit" id="SubmitButton" value="Submit"></input>
-                    </th>
-                    <th>
-                        <textarea class="Message" placeholder='Your Message (max 5000 characters)' maxlength="5000" ></textarea>                                                                                    
-                    </th>
-                    <th>
-                        <div class='contactInfo'>
-                            <p>Phone: (555) 555 5555</p>
-                            <p>Email: Email@email.com</p>
-                            <p>Address: 123 Address Street</p>
-                        </div>
-                    </th>
-                </tr>
-            </table>
-            
-        </div>
-    )
-}
+                        <input
+                            type="text"
+                            id="Name"
+                            name="name"
+                            placeholder='Your Name'
+                            value={this.state.name}
+                            onChange={this.handleInputChange}
+                        ></input><br></br>
 
-export default Contact
+                        <input
+                            type="email"
+                            id="Email"
+                            name="email"
+                            placeholder='Your Email'
+                            value={this.state.email}
+                            onChange={this.handleInputChange}
+                        ></input><br></br>
+
+                        <input
+                            type="tel"
+                            id="Phone"
+                            name="tel"
+                            placeholder='Your Phone'
+                            value={this.state.tel}
+                            onChange={this.handleInputChange}
+                        ></input><br></br>
+
+                        <input type="submit" id="SubmitButton" value="Submit"></input>
+                        </th>
+                        <th>
+                            <textarea
+                                class="Message"
+                                name="mes"
+                                placeholder='Your Message (max 5000 characters)'
+                                maxlength="5000"
+                                value={this.state.mes}
+                                onChange={this.handleInputChange}
+                            ></textarea>      
+
+                        </th>
+                        <th>
+                            <div class='contactInfo'>
+                                <p>Phone: (555) 555 5555</p>
+                                <p>Email: Email@email.com</p>
+                                <p>Address: 123 Address Street</p>
+                            </div>
+                        </th>
+                    </tr>
+                </table>
+            </form>
+        </div>
+        )
+    }
+}
