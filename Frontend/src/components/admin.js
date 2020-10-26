10b3949af528ec716027b33abfad6dbd6f366d8e
Alexander Tsarapkine <alexander.tsarapkine@gmail.com>
2020-10-26 09:56:54 -0400

    created basic adminpage

---
+++ Frontend/src/components/admin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/adminStyle.css'
+
+const Admin = () => {
+    return (
+        
+            <h1 id='AdminPageTitle'>Admin Page</h1>
+
+    )
+}
+
+export default Admin
---
+++ Frontend/src/pages/adminPage.js
@@ -0,0 +1,13 @@
+import React from "react"
+import '../styles/adminPageStyle.css'
+
+import Admin from '../components/admin'
+
+const AdminPage = () => {
+
+    return (
+      <Admin/>
+    )
+  }
+  
+  export default AdminPage
