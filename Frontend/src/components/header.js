9a4c38e8ee559b3fbcdad54ae304fdbdfcf9448d
Alexander Tsarapkine <alexander.tsarapkine@gmail.com>
2020-10-30 11:51:56 -0400

    Commented header.js

--- Frontend/src/components/header.js
+++ Frontend/src/components/header.js
@@ -1,37 +1,50 @@
-import React from 'react'
+// header
+// Handles html elements and functions for the header:
+// - When a section button is pressed, auto scroll to that section
+// - When a language is selected, change page to that language
 
-import '../styles/headerStyle.css'
-import { Link } from 'react-scroll'
+import React from 'react' // Imports react framework
+import { Link } from 'react-scroll'// Imports Link from react
 
+
+import '../styles/headerStyle.css' // Imports styling
+
+// When called, scroll to the top
 const scrollTop = () =>{
     window.scrollTo({top: 0, behavior: 'smooth'});
  };
 
+ // When called, scroll to about section
  const scrollAbout = () =>{
     let docHeight = document.documentElement.scrollHeight;
     window.scrollTo({top: docHeight*0.111, behavior: 'smooth'});
  };
 
+ // When called, scroll to services section
  const scrollServices = () =>{
     let docHeight = document.documentElement.scrollHeight;
     window.scrollTo({top: docHeight*0.245, behavior: 'smooth'});
  };
 
+ // When called, scroll to projects section
  const scrollProjects = () =>{
     let docHeight = document.documentElement.scrollHeight;
     window.scrollTo({top: docHeight*0.46, behavior: 'smooth'});
  };
 
+ // When called, scroll to process section
  const scrollProcess = () =>{
     let docHeight = document.documentElement.scrollHeight;
     window.scrollTo({top: docHeight*0.66, behavior: 'smooth'});
  };
 
+ // When called, scroll to contact section
  const scrollContact = () =>{
     let docHeight = document.documentElement.scrollHeight;
     window.scrollTo({top: docHeight, behavior: 'smooth'});
  };
 
+ // Renders header elements
 const Header = () => {
     return (
         <header id='Header' >
