9b3caaffc2278af2d9d198fd0b1773b8e201bb3b
Alexander Tsarapkine <alexander.tsarapkine@gmail.com>
2020-10-30 10:49:10 -0400

    commented index page

--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -1,19 +1,20 @@
-//react Imports
-import React from "react"
+//index
+//Stores and displays the elements for the main (index) page
 
-//JS function imports
-import Layout from '../components/layout'
-import HomeSlide from '../components/homepage'
-import About from '../components/about'
-import Services from '../components/services'
-import Projects from '../components/projects'
-import Process from '../components/process'
-import Review from '../components/review'
-import ReviewHeader from '../components/reviewHeader'
-import Contact from '../components/contact'
-import Footer from '../components/footer'
+import React from "react" //imports react framework
 
-import '../styles/indexStyle.css'
+import Layout from '../components/layout' //imports the layour element
+import HomeSlide from '../components/homepage' //imports the homepage slider element
+import About from '../components/about' // imports the about section element
+import Services from '../components/services' //imports the services section element
+import Projects from '../components/projects' // imports the projects section element
+import Process from '../components/process' // imports the process section element  
+import Review from '../components/review' // imports the review section slider element
+import ReviewHeader from '../components/reviewHeader' // imports the review section header element
+import Contact from '../components/contact' //imports the contact sedction element
+import Footer from '../components/footer' // imports the footer element
+
+import '../styles/indexStyle.css' //imports styling
 
 const IndexPage = () => {
 
