bc03d3385b89eefa880c16c09c264df963e136b4
Alexander Tsarapkine <alexander.tsarapkine@gmail.com>
2020-11-11 09:05:05 -0500

    Commented app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,31 +1,28 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv/config');
+// app.js
+// Used to connect to the mongoDB database, and host the REST API
+
+const express = require('express'); // Imports express
+const app = express();  // Stores express to app
+const mongoose = require('mongoose'); // Imports mongoose
+const bodyParser = require('body-parser'); // Imports bodyParser
+const cors = require('cors'); // Imports cors
+require('dotenv/config'); // Imports dotenv
 
 //Middlewares
-app.use(cors());
+app.use(cors()); // Tells website to use cors
 app.use(bodyParser.json());
 
 //import routes
 const infoRoute = require('./routes/infos');
 
-app.use('/', infoRoute)
+app.use('/', infoRoute);
 
-// //ROUTES (patch-update,delete-delete info,get-getinfo,post-giveinfo)
-// app.get('/', (req,res) => {
-// });
-
-
-
-//Connect to DB
+//Connects to mongoDB database
 mongoose.connect(
     process.env.DB_CONNECTION,
  { useNewUrlParser: true,
     useUnifiedTopology: true },
  () =>  console.log('connected to DB')
  )
-//listen to server
+//listen to rest API at localhost port 3000
 app.listen(3000);
